2dbfef06dd284f6169f7ba826ced401301208b11
Ebube Ezedimbu <eezedimbu@gmail.com>
2025-05-21 11:40:35 +0100

    Minor change

--- src/pages/UnderConstructionPage.jsx
+++ src/pages/UnderConstructionPage.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { Link } from 'react-router'
 const UnderConstructionPage = () => {
   return (
-    <div className='w-[803px]'>
+    <div className='max-w-[803px] mx-auto'>
         <section className="mt-[10vh] flex flex-col items-center">
           <img src="/construction.png" alt="UNDER CONSTRUCTION" className="w-150"/>
-          <Link to="/" className="text-xl py-2 px-5 bg-blue-500 text-white transition-colors duration-300 hover:cursor-pointer hover:bg-blue-600 rounded-xl mt-10"> Back To Homepage</Link>
+          <Link to="/" className="text-md py-2 px-5 bg-blue-500 text-white transition-colors duration-300 hover:cursor-pointer hover:bg-blue-600 rounded-xl mt-15"> Back To Homepage</Link>
 
         </section>
     </div>
